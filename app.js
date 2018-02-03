4fc3ed0f231a248f1004f9e2950de6a34176ddcc
ksb <kultar.bhogal@gmail.com>
2018-02-03 17:43:01 +0000

    Added Hukamnama functionality and Swagger UI

--- app.js
+++ app.js
@@ -3,11 +3,44 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').load();
 }
 
-var express = require('express');
-var scheduler = require('./workers/scheduler');
+var express         = require('express');
+var swaggerUi       = require('swagger-ui-express');
+var path            = require('path');
+var scheduler       = require('./workers/scheduler');
+var hukamnama       = require('./routes/hukamnama.js');
+var swaggerJSDoc    = require('swagger-jsdoc');
 
 var app = express();
 
+//Public resources
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Swagger config
+var swaggerSpec = swaggerJSDoc({
+    swaggerDefinition: {
+        info: {
+            title: 'Guru Nanak Darbar Gurdwara - Core services API',
+            version: '1.0.0',
+        },
+    },
+    apis: ['../routes/hukamnama.js'],
+    option:{},
+    basePath:'/',
+    host: 'localhost:3000'
+});
+
+
+// Routes
+app.get('/swagger.json', function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpec);
+});
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/hukamnama', hukamnama);
+
 scheduler.startJobs();
+
+//TODO configure error handlers
 
 module.exports = app;
---
+++ routes/hukamnama.js
@@ -0,0 +1,108 @@
+var express = require('express');
+var router = express.Router();
+var Client = require('node-rest-client').Client;
+
+var client = new Client();
+
+/**
+ * @swagger
+ * definitions:
+ *   Hukamnama:
+ *     type: object
+ *     properties:
+ *       eng:
+ *         type: array
+ *         items:
+ *           type: string
+ *       pbi:
+ *         type: array
+ *         items:
+ *           type: string
+ */
+
+/**
+ * @swagger
+ * /hukamnama/today:
+ *    get:
+ *     description: Returns today's hukamnama
+ *     produces:
+ *       - application/json
+ *     responses:
+ *          200:
+ *              description: Hukamnama for the given date
+ *              schema:
+ *                  $ref: '#/definitions/Hukamnama'
+ *
+ */
+router.get('/today', function (req, res, next) {
+    client.get("https://api.gurbaninow.com/v2/hukamnama/today", function (data, response) {
+
+        var pbiText = new Array(), engText = new Array();
+
+        for (var index in data.hukamnama) {
+            var line = data.hukamnama[index].line;
+            pbiText.push(line.gurmukhi.unicode);
+            engText.push(line.translation.english.default);
+        }
+
+        var responseData = [{"eng": engText, "pbi": pbiText}];
+
+        console.log(responseData)
+        res.json(responseData);
+    });
+});
+
+/**
+ * @swagger
+ * /hukamnama/archived/{yyyy}/{mm}/{dd}:
+ *   get:
+ *     description: Returns users
+ *     parameters:
+ *          - name: yyyy
+ *            description: year
+ *            in: path
+ *            required: true
+ *            type: integer
+ *          - name: mm
+ *            description: month
+ *            in: path
+ *            required: true
+ *            type: integer
+ *          - name: dd
+ *            description: day
+ *            in: path
+ *            required: true
+ *            type: integer
+ *     produces:
+ *          - application/json
+ *     responses:
+ *          200:
+ *              description: Hukamnama for the given date
+ *              schema:
+ *                  $ref: '#/definitions/Hukamnama'
+ */
+router.get('/archived/:yyyy/:mm/:dd', function (req, res, next) {
+    var year = req.params.yyyy;
+    var month = req.params.mm;
+    var day = req.params.dd;
+
+    var url = `https://api.gurbaninow.com/v2/hukamnama/${year}/${month}/${day}`;
+
+    client.get(url, function (data, response) {
+
+        var pbiText = new Array(), engText = new Array();
+
+        for (var index in data.hukamnama) {
+            var line = data.hukamnama[index].line;
+            pbiText.push(line.gurmukhi.unicode);
+            engText.push(line.translation.english.default);
+        }
+
+        var responseData = [{"eng": engText, "pbi": pbiText}];
+
+        console.log(responseData)
+        res.json(responseData);
+    });
+});
+
+module.exports = router;
